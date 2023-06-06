185a830e676e1e06fa57b3e08f64daa6c84453c6
yangwei999 <348134071@qq.com>
2023-06-06 15:44:53 +0800

    2

--- defect/controller/defect.go
+++ defect/controller/defect.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 
 	commonctl "github.com/opensourceways/defect-manager/common/controller"
 	"github.com/opensourceways/defect-manager/defect/app"
@@ -16,13 +17,40 @@ func AddRouteForDefectController(r *gin.RouterGroup, s app.DefectService) {
 		service: s,
 	}
 
-	r.POST("/v1/defect", ctl.Add)
+	r.POST("/v1/defect", ctl.Save)
 	r.GET("/v1/defect", ctl.Collect)
 	r.POST("/v1/defect/bulletin", ctl.GenerateBulletin)
 }
 
-func (ctl DefectController) Add(ctx *gin.Context) {
-	commonctl.SendRespOfPost(ctx, "wawa")
+// Save
+// @Summary add or update a defect
+// @Description add or update a defect
+// @Tags  Defect
+// @Accept json
+// @Param	param  body	 defectRequest	 true	"body of a defect"
+// @Success 201 {object} ResponseData
+// @Failure 400 {object} ResponseData
+// @Router /v1/defect [post]
+func (ctl DefectController) Save(ctx *gin.Context) {
+	var req defectRequest
+	if err := ctx.ShouldBindBodyWith(&req, binding.JSON); err != nil {
+		commonctl.SendBadRequestBody(ctx, err)
+
+		return
+	}
+
+	cmd, err := req.toCmd()
+	if err != nil {
+		commonctl.SendBadRequestParam(ctx, err)
+
+		return
+	}
+
+	if err := ctl.service.SaveDefects(cmd); err != nil {
+		commonctl.SendFailedResp(ctx, "", err)
+	} else {
+		commonctl.SendRespOfPost(ctx, "")
+	}
 }
 
 func (ctl DefectController) Collect(ctx *gin.Context) {
